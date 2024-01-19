605f27a2ccd150ac16b48017980fd75c6bd3d6a1
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-01-19 13:31:42 +0100

    Add a new javascript file and function

---
+++ index.js
@@ -0,0 +1,5 @@
+function actualizarOutput() {
+    let inputRange = document.getElementById("Intentos");
+    let outputRange = document.getElementById("valorIntentos");
+    outputRange.value = inputRange.value;
+  }
--- php/words.php
+++ php/words.php
@@ -4,5 +4,7 @@ $palabras = array ("php", "python", "ruby", "javascript", "java", "perl", "rust"
 echo "Un lenguaje de backend es ".$palabras[2].".";
 
 $palabra = $palabras[array_rand($palabras)];
+$oculta = str_repeat("_", strlen($palabra));
+$intentos = 8;
 
 ?>
