8c501c450efc188231166d1ec31a77f02bbde059
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-01-19 10:37:27 +0100

    Add styles and shape HTML

--- php/words.php
+++ php/words.php
@@ -3,5 +3,6 @@
 $palabras = array ("php", "python", "ruby", "javascript", "java", "perl", "rust");
 echo "Un lenguaje de backend es ".$palabras[2].".";
 
+$palabra = $palabras[array_rand($palabras)];
 
 ?>
