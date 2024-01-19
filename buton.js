7817b75defd31fd4f6b1b9caf3691a232f1bf80b
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-01-19 14:06:46 +0100

    add play button

--- buton.js
+++ buton.js
@@ -1,11 +1,4 @@
-
-function actualizarOutput() {
-    let inputRange = document.getElementById("Intentos");
-    let outputRange = document.getElementById("valorIntentos");
-    outputRange.value = inputRange.value;
-  }
-
-  let botonJugar = document.getElementById("jugar");
+let botonJugar = document.getElementById("jugar");
 // Obtener el elemento div por su clase
 let mainContainer = document.getElementsByClassName("main-container")[0];
 // Añadir un evento onclick al botón
